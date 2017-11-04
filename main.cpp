3a1dea8e4aca7dbcf05a191cbfcc6271440de891
JamesC403 <jamesmail403@yahoo.com>
2017-11-04 19:56:42 -0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 
 using namespace std;
-
+/*
+ * TODO- Functions!
+ */
 int main() {
-
+        /*
+         * TODO- preferably a switch statement with access to each of the functions that are created to fit the different demands of this project
+         */
     return 0;
 }
