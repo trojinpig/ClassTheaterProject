c1fccf61f1411616a46ad88b16cf317d02a8655b
JamesC403 <jamesmail403@yahoo.com>
2017-11-04 20:12:51 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ using namespace std;
  * TODO- Functions!
  */
 int main() {
+    //Constant for your rows and constant for your seats with a 2d array pre-defined.
+    const ROWS = 15;
+    const SEATS = 30;
+    int seatIndex [ROWS][SEATS];
         /*
          * TODO- a switch statement to call the functions depending on user input
          */
