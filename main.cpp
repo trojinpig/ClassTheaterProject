8928c69532ffab09fd0ef4b57c049ef6bff0c0e3
JamesC403 <jamesmail403@yahoo.com>
2017-11-04 23:37:23 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,11 +4,42 @@ using namespace std;
 /*
  * TODO- Functions!
  */
+int totalSales() {
+    int allSales;
+    //function to keep a tally of total seats sold.
+    return allSales;
+}
+
+int unsoldCount () {
+    int freeSeats;
+    // function to return the amount of unsold/free seats total.
+    return freeSeats;
+}
+
+int soldCount () {
+    int soldSeats;
+    //function to return the amount of seats sold total.
+    return soldSeats;
+}
+
 int main() {
     //Constant for your rows and constant for your seats with a 2d array pre-defined.
     const ROWS = 15;
     const SEATS = 30;
     int seatIndex [ROWS][SEATS];
+    int usrInput; //TODO write code to recieve user input
+    switch (usrInput) {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        default:
+            break;
+    }
         /*
          * TODO- a switch statement to call the functions depending on user input.
          */
