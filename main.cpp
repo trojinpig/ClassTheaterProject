f1b8a7d4e8e298ba1d3ea5c0aeca9866ff984afe
trojinpig <33333889+trojinpig@users.noreply.github.com>
2017-11-07 15:51:41 -0500

    Update main.cpp
    asking for the rows and seats
    ties the price to the seat

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int totalSales() {
     int allSales;
     //function to keep a tally of total seats sold.
+    allSales += 1;
     return allSales;
 }
 
@@ -16,13 +17,100 @@ int unsoldCount () {
     return freeSeats;
 }
 
-int soldCount () {
-    int soldSeats;
-    //function to return the amount of seats sold total.
-    return soldSeats;
-}
+#include <iostream>
+using namespace std;
 
-int main() {
+// asking for the rows
+int row = 0;
+int getrow()
+{
+	while (row == 0)
+	{
+		cout << "plesa input Row number 1-15" << endl;
+		cin >> row;
+		//cheking the input number is a proper
+		//TODO:this needs to ties 1-15 to A-O 
+		if (row >= 1 && row <= 15)
+		{
+			return row;
+		}
+		else 
+		{
+			cout << "false input: pleas input a proper input" << endl;
+			cout << endl;
+			row = 0;
+		}
+	}
+}
+//geting the seats
+int seat = 0;
+int getseat()
+{
+	while (seat == 0)
+	{
+		cout << "plesa input Row number 1-30" << endl;
+		cin >> seat;
+		if (seat >= 1 && seat <= 30)
+		{
+			return seat;
+		}
+		else
+		{
+			cout << "false input: pleas input a proper input" << endl;
+			cout << endl;
+			seat = 0;
+		}
+	}
+	
+}
+//ties the prise to the seat
+double price;
+int getprice()
+{
+	if (row >= 1 && row <= 5)
+	{
+		price = 350;
+	}
+	else if (row >= 6 && row <= 10)
+	{
+		price = 250;
+	}
+	else if (row >= 11 && row <= 15)
+	{
+		price = 150;
+	}
+	return price;
+}
+	//geting a respons frome the user 
+int check = 2;
+int allSales;
+	int response()
+	{
+		cout << "This seat will cost you: $" << price << endl;
+		cout << "Would you like to purchase this seat? ENTER 1 = Yes, 2 = No ";
+		cin >> check;
+		if (check == 1)
+		{
+			cout << "thank you your seat is" << row << ":" << seat << endl;
+			return check;
+		}
+		else if (check == 2)
+		{
+            cout << ""
+		}
+	}
+
+
+
+int main() 
+{
+		int row = getrow();
+		int seat = getseat();
+		//TODO: create a methed that checks the aray for the row and seat to see if ther taken
+		int price = getprice();
+		int check = response();
+		cout << check;
+
     //Constant for your rows and constant for your seats with a 2d array pre-defined.
     const ROWS = 15;
     const SEATS = 30;
